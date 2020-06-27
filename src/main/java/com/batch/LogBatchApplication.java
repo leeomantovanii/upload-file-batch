140672807be8843796b020a400e0ba559324509b
Leonardo <leeomantovanii@gmail.com>
2020-06-27 12:06:11 -0300

    Cria a estrutura do projeto

---
+++ src/main/java/com/batch/LogBatchApplication.java
@@ -0,0 +1,13 @@
+package com.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogBatchApplication.class, args);
+	}
+
+}
