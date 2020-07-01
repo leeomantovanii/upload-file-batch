73c340cb60c4476ab7a2df76b023d81f908a9b8b
Leonardo <leeomantovanii@gmail.com>
2020-07-01 20:35:28 -0300

    Adiciona todos serviços de crud na controller e cria testes unitários

--- src/main/java/com/batch/presenter/rest/controller/LogBatchController.java
+++ src/main/java/com/batch/presenter/rest/controller/LogBatchController.java
@@ -1,12 +1,12 @@
 package com.batch.presenter.rest.controller;
 
-import com.batch.core.InserirLogBatchUseCase;
-import com.batch.core.InserirLogUseCase;
+import com.batch.core.*;
 import com.batch.core.models.DadosLog;
 import com.batch.presenter.rest.request.LogRequest;
 import com.batch.presenter.rest.request.mapper.LogBatchRequestMapper;
 import com.batch.presenter.rest.response.LogResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +14,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * Controller responsável por realizar operações CRUD de log
@@ -32,13 +34,19 @@ public class LogBatchController {
 
     private final InserirLogUseCase inserirLogUseCase;
 
+    private final DeletarLogUseCase deletarLogUseCase;
+
+    private final PesquisarLogUseCase pesquisarLogUseCase;
+
+    private final AtualizarLogUseCase atualizarLogUseCase;
+
     /**
      * Serviço responsável por fazer inserção em batch
      *
      * @param arquivoLog
      * @return
      */
-    @PostMapping (path = "/batch")
+    @PostMapping(value = "/batch")
     public ResponseEntity inserirLogBatch(@RequestParam("arquivoLog") MultipartFile arquivoLog) {
 
         try {
@@ -52,6 +60,7 @@ public class LogBatchController {
 
     /**
      * Serviço responsável por fazer inserção de log manual
+     *
      * @param request
      * @return
      */
@@ -59,6 +68,33 @@ public class LogBatchController {
     public ResponseEntity<LogResponse> inserirLog(@RequestBody @Valid LogRequest request) {
 
         DadosLog retorno = inserirLogUseCase.insereLog(mapper.requestToCore(request));
+
+        return new ResponseEntity(mapper.coreToResponse(retorno), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/deletar/{id_log}")
+    public ResponseEntity deletarLog(@PathVariable("id_log") final String idLog) {
+
+        deletarLogUseCase.deletarLog(idLog);
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LogResponse>> buscarLogs(@RequestParam(required = false, name = "ip") String ip,
+                                                        @RequestParam(required = false, name = "dt_inicio") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dtInicio,
+                                                        @RequestParam(required = false, name = "dt_fim")  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dtFim) {
+
+        List<DadosLog> result = pesquisarLogUseCase.pesquisarLog(ip, dtInicio, dtFim);
+
+        return new ResponseEntity(mapper.listCoreToListResponse(result), HttpStatus.OK);
+
+    }
+
+    @PutMapping
+    public ResponseEntity<LogResponse> atualizaLog(@RequestBody @Valid LogRequest request) {
+
+        DadosLog retorno = atualizarLogUseCase.atualizarLog(mapper.requestToCore(request));
 
         return new ResponseEntity(mapper.coreToResponse(retorno), HttpStatus.OK);
     }
--- src/test/java/com/batch/presenter/rest/controller/LogBatchControllerTest.java
+++ src/test/java/com/batch/presenter/rest/controller/LogBatchControllerTest.java
@@ -1,7 +1,6 @@
 package com.batch.presenter.rest.controller;
 
-import com.batch.core.InserirLogBatchUseCase;
-import com.batch.core.InserirLogUseCase;
+import com.batch.core.*;
 import com.batch.core.models.DadosLog;
 import com.batch.presenter.rest.request.LogRequest;
 import com.batch.presenter.rest.request.mapper.LogBatchRequestMapper;
@@ -12,12 +11,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 import teste.Podam;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -41,23 +45,33 @@ class LogBatchControllerTest {
     @Mock
     private InserirLogUseCase inserirLogUseCase;
 
+    @Mock
+    private DeletarLogUseCase deletarLogUseCase;
+
+    @Mock
+    private PesquisarLogUseCase pesquisarLogUseCase;
+
+    @Mock
+    private AtualizarLogUseCase atualizarLogUseCase;
+
     @Test
-    @DisplayName("Verifica se o serviço esta chamando a camada de negocio (useCase)")
-    void verificaChamadaCamadaNegocioinserirLogBatch() throws IOException {
+    @DisplayName("Verifica se o serviço esta chamando a camada de negocio (useCase) e se o status http é 200")
+    void testaInserirLogBatch() throws IOException {
         MultipartFile fileLog = Podam.MOCKS.manufacturePojo(MultipartFile.class);
         Reader reader = Podam.MOCKS.manufacturePojo(Reader.class);
 
         when(mapper.requestBatchToCore(fileLog)).thenReturn(reader);
 
-        controller.inserirLogBatch(fileLog);
+        ResponseEntity retorno = controller.inserirLogBatch(fileLog);
 
         verify(inserirLogBatchUseCase, times(1)).processarArquivo(reader);
+        assertThat(retorno.getStatusCode()).isEqualTo(HttpStatus.OK);
 
     }
 
     @Test
-    @DisplayName("Verifica se o serviço esta chamando a camada de negocio (useCase) ao inserir log manual")
-    void verificaChamadaCamadaNegocioinserirLog() throws IOException {
+    @DisplayName("Verifica se o serviço esta chamando a camada de negocio (useCase) ao inserir log manual, se o objeto retornado é o esperado e se o status htpp é 200")
+    void testaInserirLog() {
         LogRequest request = Podam.MOCKS.manufacturePojo(LogRequest.class);
         DadosLog core = Podam.MOCKS.manufacturePojo(DadosLog.class);
         LogResponse response = Podam.MOCKS.manufacturePojo(LogResponse.class);
@@ -66,9 +80,54 @@ class LogBatchControllerTest {
         when(mapper.coreToResponse(core)).thenReturn(response);
         when(inserirLogUseCase.insereLog(core)).thenReturn(core);
 
-        controller.inserirLog(request);
+        ResponseEntity<LogResponse> retorno = controller.inserirLog(request);
 
         verify(inserirLogUseCase, times(1)).insereLog(core);
+        assertThat(retorno.getBody()).isEqualTo(response);
+        assertThat(retorno.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    @DisplayName("Verifica se o serviço esta chamando a camada de negocio (useCase) ao deletar log e se é retornado status http 200")
+    void testaDeletarLog() {
+        ResponseEntity retorno = controller.deletarLog("10");
+        verify(deletarLogUseCase, times(1)).deletarLog("10");
+        assertThat(retorno.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    @DisplayName("Verifica se o serviço esta chamando a camada de negocio (useCase), se retorna uma lista de objeto e se o status http é 200")
+    void testaBuscarLogs() {
+        List<DadosLog> retornoUseCase = Podam.MOCKS.manufacturePojo(ArrayList.class, DadosLog.class);
+        List<LogResponse> retornoMaper = Podam.MOCKS.manufacturePojo(ArrayList.class, LogResponse.class);
+
+        when(pesquisarLogUseCase.pesquisarLog(any(), any(), any())).thenReturn(retornoUseCase);
+        when(mapper.listCoreToListResponse(any())).thenReturn(retornoMaper);
+
+        ResponseEntity<List<LogResponse>> retorno = controller.buscarLogs("10", null, null);
+
+        verify(pesquisarLogUseCase, times(1)).pesquisarLog("10", null, null);
+        assertThat(retorno.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(retorno.getBody()).isEqualTo(retornoMaper);
+    }
+
+    @Test
+    @DisplayName("Verifica se o serviço esta chamando a camada de negocio (useCase), se retorna o objeto atualizado e se o status http é 200")
+    void testaAtualizaLog() {
+        LogRequest request = Podam.MOCKS.manufacturePojo(LogRequest.class);
+        DadosLog core = Podam.MOCKS.manufacturePojo(DadosLog.class);
+        LogResponse response = Podam.MOCKS.manufacturePojo(LogResponse.class);
+
+        when((mapper.requestToCore(request))).thenReturn(core);
+        when(mapper.coreToResponse(core)).thenReturn(response);
+        when(atualizarLogUseCase.atualizarLog(core)).thenReturn(core);
+
+        ResponseEntity<LogResponse> retorno = controller.atualizaLog(request);
+
+        verify(atualizarLogUseCase, times(1)).atualizarLog(core);
+        assertThat(retorno.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(retorno.getBody()).isEqualTo(response);
+
 
     }
 
